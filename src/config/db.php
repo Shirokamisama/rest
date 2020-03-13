81ae52b23c1fdabf9545ab38b88375667d53746f
Shirokamisama <61605987+Shirokamisama@users.noreply.github.com>
2020-03-13 12:47:01 -0500

    Update db.php
    Cambiar la ruta de acceso a la base de datos

--- src/config/db.php
+++ src/config/db.php
@@ -2,9 +2,9 @@
     class db{
         // Properties
         private $dbhost = 'remotemysql.com';
-        private $dbuser = 'y64MtsQZL5';
-        private $dbpass = 'ASbmFQRbdB';
-        private $dbname = 'y64MtsQZL5';
+        private $dbuser = 'w26OeUrpvR';
+        private $dbpass = 'JMqv5rx8xS';
+        private $dbname = 'w26OeUrpvR';
 
         // Connect
         public function connect(){
