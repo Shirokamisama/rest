be8bdc854103c4cb446ad058deeabe4f06d858a3
Shirokamisama <61605987+Shirokamisama@users.noreply.github.com>
2020-03-13 19:00:05 -0500

    Update customers.php

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -17,8 +17,8 @@ $app->add(function ($req, $res, $next) {
 });
 
 // Get All Customers
-$app->get('/api/customers', function(Request $request, Response $response){
-    $sql = "SELECT * FROM customers";
+$app->get('/api/series', function(Request $request, Response $response){
+    $sql = "SELECT * FROM Series";
 
     try{
         // Get DB Object
@@ -36,10 +36,10 @@ $app->get('/api/customers', function(Request $request, Response $response){
 });
 
 // Get Single Customer
-$app->get('/api/customer/{id}', function(Request $request, Response $response){
+$app->get('/api/serie/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
 
-    $sql = "SELECT * FROM customers WHERE id = $id";
+    $sql = "SELECT * FROM Series WHERE id = $id";
 
     try{
         // Get DB Object
